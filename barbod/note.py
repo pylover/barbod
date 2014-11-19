272e2dff288bf6efe1f6085bc0a8a39f141667f8
pylover <vahid.mardani@gmail.com>
2014-11-19 06:20:43 +0330

    constants module

--- barbod/note.py
+++ barbod/note.py
@@ -1,42 +1,9 @@
 # -*- coding: utf-8 -*-
-__author__ = 'vahid'
 
 import re
+from barbod.constants import NOTE_NAMES, NOTE_OFFSETS
 
-_NOTE_OFFSETS = {
-    'C': 0,
-    'C#': 1,
-    'Db': 1,
-    'D': 2,
-    'D#': 3,
-    'Eb': 3,
-    'E': 4,
-    'F': 5,
-    'F#': 6,
-    'Gb': 6,
-    'G': 7,
-    'G#': 8,
-    'Ab': 8,
-    'A': 9,
-    'A#': 10,
-    'Bb': 10,
-    'B': 11
-}
-
-_NOTE_NAMES = {
-    0: ('C',),
-    1: ('C#', 'Db'),
-    2: ('D',),
-    3: ('D#', 'Eb'),
-    4: ('E',),
-    5: ('F',),
-    6: ('F#', 'Gb'),
-    7: ('G',),
-    8: ('G#', 'Ab'),
-    9: ('A',),
-    10: ('A#', 'Bb'),
-    11: ('B',)
-}
+__author__ = 'vahid'
 
 
 class Note(object):
@@ -55,7 +22,7 @@ class Note(object):
             raise ArgumentError('Invalid note name: %s' % expr)
 
         try:
-            index_in_octave = _NOTE_OFFSETS[match['note']]
+            index_in_octave = NOTE_OFFSETS[match['note']]
         except KeyError:
             raise ArgumentError('Invalid note name: %s' % expr)
 
@@ -64,21 +31,21 @@ class Note(object):
         else:
             octave = 0
 
-        return octave * len(_NOTE_NAMES) + index_in_octave
+        return octave * len(NOTE_NAMES) + index_in_octave
 
     @property
     def octave(self):
-        return self._c_offset // len(_NOTE_NAMES)
+        return self._c_offset // len(NOTE_NAMES)
 
     @property
     def index(self):
-        return self._c_offset % len(_NOTE_NAMES)
+        return self._c_offset % len(NOTE_NAMES)
 
     def __str__(self):
-        return '%s%s' % (_NOTE_NAMES[self.index][0], '' if self.octave == 0 else self.octave)
+        return '%s%s' % (NOTE_NAMES[self.index][0], '' if self.octave == 0 else self.octave)
 
     def __repr__(self):
-        return ' '.join(['%s%s' % (n, '' if self.octave == 0 else self.octave) for n in _NOTE_NAMES[self.index]])
+        return ' '.join(['%s%s' % (n, '' if self.octave == 0 else self.octave) for n in NOTE_NAMES[self.index]])
 
     def __recognize_other(self, other):
         return other if not isinstance(other, Note) else other._c_offset
